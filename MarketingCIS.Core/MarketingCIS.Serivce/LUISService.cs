27582100f7f02d3489cf63e3b06208c08286dbb3
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-04-26 12:28:52 +0200

    Add initial Luis Service

---
+++ MarketingCIS.Core/MarketingCIS.Serivce/LUISService.cs
@@ -0,0 +1,43 @@
+﻿using MarketingCIS.Serivce.CIS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace MarketingCIS.Serivce
+{
+    public partial class LUISService : ServiceBase
+    {
+        private Task task;
+        private CancellationToken cancelToken;
+
+        public LUISService()
+        {
+            InitializeComponent();
+            cancelToken = new CancellationToken();
+            
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            task = Task.Run(() => Process(), cancelToken);
+        }
+
+        protected override void OnStop()
+        {
+            
+        }
+        
+        private void Process()
+        {
+
+        }
+    }
+}
