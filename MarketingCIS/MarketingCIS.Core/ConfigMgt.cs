1615c1a61af13b4c72be6da47ac7aac97661eed8
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-04-28 12:39:28 +0200

    Add Json and file handling

---
+++ MarketingCIS/MarketingCIS.Core/ConfigMgt.cs
@@ -0,0 +1,27 @@
+﻿using MarketingCIS.Core.JSON;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketingCIS.Core
+{
+    static class ConfigMgt
+    {
+        private static Config config;
+        private static FileInfo configFile;
+
+        public static void GetConfigFrom(string fullName)
+        {
+            configFile = new FileInfo(fullName);
+            config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(fullName));
+        }
+
+        public static void ApplyChangesToFile() =>
+            File.WriteAllText(configFile.FullName, JsonConvert.SerializeObject(config, Formatting.Indented));
+
+    }
+}
