c4f218b68e32032c27128e7fa46f956fea7f4d04
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-04-28 12:39:13 +0200

    Add testservice for testing services

---
+++ MarketingCIS/TestSerivce/LUISService.cs
@@ -0,0 +1,40 @@
+﻿using MarketingCIS.Core.CognitiveServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestSerivce
+{
+    class LUISService
+    {
+        private Task task;
+        private CancellationTokenSource cancelTokenSource;
+        private LUISClient luisClient;
+
+        public LUISService()
+        {
+            //InitializeComponent();
+            cancelTokenSource = new CancellationTokenSource();
+            luisClient = new LUISClient("", "");
+        }
+
+        public  void OnStart(string[] args)
+        {
+            task = Task.Run(() => Process(), cancelTokenSource.Token);
+        }
+
+        public  void OnStop()
+        {
+            cancelTokenSource.Cancel(false);
+        }
+
+        private void Process()
+        {
+
+        }
+    }
+}
+
