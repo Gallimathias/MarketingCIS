51e94205a22759484d688c3d80da3d4e4351ad6e
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-05-23 15:26:00 +0200

    Add setting handler

---
+++ TheMarketingPlatform/TheMarketingPlatform.Service/SettingsHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMarketingPlatform.Service
+{
+    class SettingsHandler
+    {
+        public SettingsHandler()
+        {
+            Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Gallimathias\TheMarketingPlatform\Main", "", null);
+        }
+    }
+}
