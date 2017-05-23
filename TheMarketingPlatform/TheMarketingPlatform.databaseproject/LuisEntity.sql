88fa53fbb55d5d0e9672e91825a996a5c6341c24
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-05-23 12:23:05 +0200

    Add luis response structure

---
+++ TheMarketingPlatform/TheMarketingPlatform.databaseproject/LuisEntity.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[LuisEntity]
+(
+	[LuisResponseId] INT NOT NULL, 
+    [Id] INT NOT NULL IDENTITY, 
+    [Entity] NVARCHAR(MAX) NOT NULL, 
+    [Type] NVARCHAR(MAX) NOT NULL, 
+    [StartIndex] INT NULL, 
+    [EndIndex] INT NULL, 
+    [Score] FLOAT NOT NULL, 
+    CONSTRAINT [FK_LuisEntity_LuisResponse] FOREIGN KEY (LuisResponseId) REFERENCES dbo.LuisResponse(id), 
+    CONSTRAINT [PK_LuisEntity] PRIMARY KEY (LuisResponseId, Id) 
+)
--- TheMarketingPlatform/TheMarketingPlatform.databaseproject/MailReceiver.sql
+++ TheMarketingPlatform/TheMarketingPlatform.databaseproject/MailReceiver.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[MailReceiver]
 (
-	[Id] INT NOT NULL, 
+	[MailId] INT NOT NULL, 
     [Receiver] NVARCHAR(254) NOT NULL, 
-    CONSTRAINT [FK_MailReceiver_Mail] FOREIGN KEY (Id) REFERENCES dbo.Mail(Id), 
-    CONSTRAINT [PK_MailReceiver] PRIMARY KEY (Id,Receiver) 
+    CONSTRAINT [FK_MailReceiver_Mail] FOREIGN KEY ([MailId]) REFERENCES dbo.Mail(Id), 
+    CONSTRAINT [PK_MailReceiver] PRIMARY KEY ([MailId],Receiver) 
 )
