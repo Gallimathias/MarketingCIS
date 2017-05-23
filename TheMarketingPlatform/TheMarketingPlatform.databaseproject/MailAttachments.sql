9f866cbd8c65198a9ede40808189c10458614dc8
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-05-23 12:58:21 +0200

    Change name from MailAttachmants to correct Nameconventions

--- TheMarketingPlatform/TheMarketingPlatform.databaseproject/MailAttachments.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[MailAttachments]
-(
-	[MailId] INT NOT NULL, 
-    [Id] INT NOT NULL IDENTITY, 
-    [Attachment] VARBINARY(MAX) NOT NULL, 
-    [FileExtension] NVARCHAR(260) NULL, 
-    CONSTRAINT [FK_MailAttachments_Mail] FOREIGN KEY (MailId) REFERENCES dbo.Mail(Id), 
-    CONSTRAINT [PK_MailAttachments] PRIMARY KEY (MailId,Id) 
-)
