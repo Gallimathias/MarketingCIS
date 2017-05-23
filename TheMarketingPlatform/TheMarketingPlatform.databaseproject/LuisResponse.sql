3498ec8a40d7d3810d366a4f539358ad57fa6ca6
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-05-23 12:29:33 +0200

    Change primary key in luisResponse

--- TheMarketingPlatform/TheMarketingPlatform.databaseproject/LuisResponse.sql
+++ TheMarketingPlatform/TheMarketingPlatform.databaseproject/LuisResponse.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL IDENTITY, 
 	[MailId] INT NOT NULL, 
     [TimeStamp] DATETIME NOT NULL, 
-    CONSTRAINT [PK_LuisResponse] PRIMARY KEY (Id, MailId), 
+    CONSTRAINT [PK_LuisResponse] PRIMARY KEY (Id), 
     CONSTRAINT [AK_LuisResponse_MailId_TimeStamp] UNIQUE (MailId,TimeStamp), 
     CONSTRAINT [FK_LuisResponse_Mail] FOREIGN KEY (MailId) REFERENCES dbo.Mail(Id)
 )
