36138b36e79059a03210d0a4489c890f630b1ec3
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-05-23 13:44:03 +0200

    Add forgotten using

--- TheMarketingPlatform/TheMarketingPlatform.Service/LUISService.cs
+++ TheMarketingPlatform/TheMarketingPlatform.Service/LUISService.cs
@@ -1,6 +1,7 @@
 ﻿using System.ServiceProcess;
 using System.Threading;
 using System.Threading.Tasks;
+using TheMarketingPlatform.Core;
 using TheMarketingPlatform.Core.CognitiveServices;
 
 namespace TheMarketingPlatform.Service
